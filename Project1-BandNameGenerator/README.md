b636ad50f5f9455dca818c268ca612287a2a07b6
Akash <proakash256@gmail.com>
2023-10-02 01:35:52 +0530

    Initial Commit

---
+++ Project1-BandNameGenerator/README.md
@@ -0,0 +1,3 @@
+# BandNameGenerator
+
+This project takes the name of your City and your pet's name as Input and combines them to form your Band's Name. How cool is that !!
---
+++ Project1-BandNameGenerator/main.py
@@ -0,0 +1,4 @@
+print("Welcome! To the Band Name Generator...\n")
+city = input("What's your City's name ??\n")
+pet = input("What's your Pet's name ??\n")
+print("Your Band's Name could be " + city + " " + pet)
