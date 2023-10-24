19233f81313c660e7e8bddbdaba42188daf04e1e
Akash <proakash256@gmail.com>
2023-10-24 10:01:25 +0530

    Initial Commit

---
+++ Project17-Quiz/data.py
@@ -0,0 +1,35 @@
+question_data = [
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "The common software-programming acronym &quot;I18N&quot; comes from the term "
+                 "&quot;Inter localization&quot;.",
+     "correct_answer": "False",
+     "incorrect_answers": ["True"]
+     },
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "The first computer bug was formed by faulty wires.", "correct_answer": "False",
+     "incorrect_answers": ["True"]
+     },
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "The very first recorded computer &quot;bug&quot; was a moth found inside a Harvard Mark II computer.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "All program codes have to be compiled into an executable file in order to be run. This file can "
+                 "then be executed on any machine.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "&quot;Windows NT&quot; is a monolithic kernel.", "correct_answer": "False",
+     "incorrect_answers": ["True"]}, {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+                                      "question": "MacOS is based on Linux.", "correct_answer": "False",
+                                      "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "The last Windows operating system to be based on the Windows 9x kernel was Windows 98.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "It&#039;s not possible to format a write-protected DVD-R Hard Disk.", "correct_answer": "True",
+     "incorrect_answers": ["False"]}, {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+                                       "question": "Android versions are named in alphabetical order.",
+                                       "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "A Boolean value of &quot;0&quot; represents which of these words?", "correct_answer": "False",
+     "incorrect_answers": ["True"]}
+]
